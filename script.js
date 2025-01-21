e4b21f99aec30203adf40d1409e050f16d3490f8
bishalxrauniyar <Bishal.rauniyar@gmail.com>
2025-01-21 17:48:14 +0545

    revision js

--- script.js
+++ script.js
@@ -1310,27 +1310,6 @@ console.log(divide(81, 3)); // 1
 console.log(divide(100, 2)); // 25
 console.log(divide(100, 3)); // 100
 
-//JavaScript program to find the number of sorted pairs formed by arrays of integers. This is such that one element in the pair is divisible by the other one.
-// For example - The output of [1, 3, 2] ->2 - (1,3), (1,2).
-// The output of [2, 4, 6] -> 2 - (2,4), (2,6)
-// The output of [2, 4, 16] -> 3 - (2,4), (2,16), (4,16)
-function sortedPairs(arr) {
-  let count = 0;
-
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = i + 1; j < arr.length; j++) {
-      if (arr[i] % arr[j] === 0 || arr[j] % arr[i] === 0) {
-        count++;
-      }
-    }
-  }
-
-  return count;
-}
-console.log(sortedPairs([1, 3, 2])); // 2
-console.log(sortedPairs([2, 4, 6])); // 2
-console.log(sortedPairs([2, 4, 16])); // 3
-
 //a JavaScript program to create the dot products of two given 3D vectors.
 // Note: The dot product is the sum of the products of the corresponding entries of the two sequences of numbers.
 

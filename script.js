f7788bae96ae41a8bacace5d27fe2609fd1bdb5b
bishalxrauniyar <Bishal.rauniyar@gmail.com>
2025-02-20 17:46:04 +0545

    d

--- script.js
+++ script.js
@@ -934,7 +934,7 @@ function firstlastarray(array) {
 }
 console.log(firstlastarray([1, 2, 3])); // [1, 3]
 
-//program to test whether an array of integers of length 2 contains 1 or 3.
+//program to test whether an array of integers of length 2 contains 1 or 3
 function oneorthree(array) {
   if (array.length === 2) {
     if (array[0] === 1 || array[0] === 3 || array[1] === 1 || array[1] === 3) {
